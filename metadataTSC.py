ded74861f7a4314e8d72de65d3880b5d314d7ab7
gembox <rhelenius@gmail.com>
2022-04-30 16:51:54 -0400

    adding sample TSC metadata api call

---
+++ metadataTSC.py
@@ -0,0 +1,43 @@
+import csv
+import os
+import json
+import tableauserverclient as TSC
+
+with open('C:\\creds\\pat.txt', 'r') as file :
+    token = file.read()
+
+with open('C:\\creds\\name.txt', 'r') as file :
+    user = file.read()
+
+with open('C:\\creds\\site.txt', 'r') as file :
+    site = file.read()
+
+tableau_auth = TSC.PersonalAccessTokenAuth(user,token,site)
+server = TSC.Server('https://10ax.online.tableau.com', use_server_version=True)
+
+with server.auth.sign_in(tableau_auth):
+    result = server.metadata.query("""
+    query tables {
+  tables (filter:{name:"Orders"}){
+    name
+    id
+    columns (filter:{name:"Segment"}){
+      id
+      name
+      downstreamDashboards{
+        name
+        luid
+        path
+      }
+    }
+  }
+}
+    """)
+    if result.get("errors"):
+        print('### Errors/Warnings')
+        print(result['errors'])
+    
+    if result.get("data"):
+        with open('query_result.json',"w") as jsonfile:
+            json.dump(result['data'], jsonfile, indent =4)
+
